3d9077b9ee6a408cbdc954c9676df57410d64cf4
bigface <jiangh_wu@163.com>
2019-07-10 08:40:50 +0800

    消除了Django代码带来的异味

--- sementic_server/api.py
+++ sementic_server/api.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
+"""
+@description: Django Api
+@author: Wu Jiang-Heng
+@email: jiangh_wu@163.com
+@time: 2019-07-09
+@version: 0.0.1
+"""
 
 from django.views.decorators.csrf import csrf_exempt
 from sementic_server.source.intent_extraction.item_matcher import ItemMatcher
